d1c054972a6e906ecb765cc8d7e0b0a3813afe28
João Uzeda <joaouzeda.dev@gmail.com>
2026-08-13 21:43:30 -0300

    Atualizar o PrincipalComLista.java

--- src/main/java/screenmatch/principal/PrincipalComLista.java
+++ src/main/java/screenmatch/principal/PrincipalComLista.java
@@ -24,7 +24,7 @@ public class PrincipalComLista {
 	private ConsumoApi consumo = new ConsumoApi();
 	private ConverteDados conversor = new ConverteDados();
 	private final String ENDERECO = "https://www.omdbapi.com/?t=";
-	private final String API_KEY = "&apikey=6585022c";
+	private final String API_KEY = "&apikey="SUA API KEY"";
 	private List<DadosSerie> dadosSeries = new ArrayList<>(); 
 	private SerieRepository repositorio;
 	private List<Serie> series = new ArrayList<>();
